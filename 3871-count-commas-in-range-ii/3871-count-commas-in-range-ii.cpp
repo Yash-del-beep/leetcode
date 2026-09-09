acb15a483616562728fddf3dd675467fdc3dd889
Yash Srivastav <ysrivasta987@gmail.com>
2026-09-09 19:12:57 +0530

    Time: 0 ms (100%), Space: 9 MB (57.33%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    int64_t pow[6] = {1000, 1000000, 1000000000, 1000000000000, 1000000000000000, 1000000000000000000};
-    
-    int64_t countCommas(auto& n) {
-        int64_t k = 0;        
-        for (auto& p : pow) k += n >= p;
-        
-        return k * (n + 1) - (pow[k] - 1000) / 999;
+    long long countCommas(long long n) {
+        long long curr=1000;
+        long long res=0;
+        while(curr<=n){
+            res+=n-curr+1;
+            curr*=1000;
+        }
+        return res;
     }
-};
+}; 
